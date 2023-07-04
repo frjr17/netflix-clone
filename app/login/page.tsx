a6f06dfb9c1ce0751763562f4465e28d35e088b6
Hernan Valencia <frjr17@criptext.com>
2023-07-04 17:59:01 -0500

    Building login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,19 +7,30 @@ import {
   Input,
   Button,
   FormLabel,
+  LightMode,
 } from "@chakra-ui/react";
 
 export default function Login() {
   return (
-    <Center>
-      <VStack>
-        <Heading>Sign In</Heading>
-        <FormControl>
-          <FormLabel>Email Adress</FormLabel>
-          <Input />
-        </FormControl>
-        <Button>Sign In</Button>
-      </VStack>
+    <Center backgroundImage={"/images/signin-bg.jpeg"} height={"100vh"}>
+      <Center background={"rgba(0,0,0,0.3)"} width={"100vw"} height={"100vh"}>
+        <VStack
+          background={"rgba(0,0,0,0.7)"}
+          width={"350px"}
+          height={"350px"}
+          padding={7}
+          justify={"space-around"}
+        >
+          <Heading>Sign In</Heading>
+          <FormControl>
+            <FormLabel>Email Adress</FormLabel>
+            <Input />
+          </FormControl>
+          <LightMode>
+            <Button colorScheme="red">Sign In</Button>
+          </LightMode>
+        </VStack>
+      </Center>
     </Center>
   );
 }
