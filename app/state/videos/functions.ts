180582110376d3cdd003ff94be88c460aed4fb55
Hernan Valencia <frjr17@criptext.com>
2023-07-08 12:28:21 -0500

    Creating getVideo Function

--- app/state/videos/functions.ts
+++ app/state/videos/functions.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useVideosState } from ".";
-import { VideoObject } from "@/app/lib/videos";
+import { VideoObject, YoutubeVideo } from "@/app/lib/videos";
 
 type TTest = {
   disney: VideoObject[];
@@ -30,4 +30,45 @@ export const orderAll = (videos: TTest) => {
     ["Travel", videos.travel],
     ["Landscapes", videos.landscapes],
   ];
+};
+
+/*The properties of a video.
+@interface
+@property {string} title - The title of the video.
+@property {string} publishTime - The publish time of the video.
+@property {string} description - The description of the video.
+@property {string} channelTitle - The title of the video's channel.
+@property {string} imgUrl - The URL of the video's image.
+@property {number} viewCount - The number of views of the video.
+*/
+interface VideoProps {
+  title: string;
+  publishTime: string;
+  description: string;
+  channelTitle: string;
+  imgUrl: string;
+  viewCount: number;
+}
+
+export const getVideo = async (videoId: string) => {
+  const videosState = useVideosState;
+  videosState.setState({ isFetching: true });
+
+  const response = await axios<YoutubeVideo>({
+    method: "GET",
+    url: "/api/getVideoById",
+    params: { videoId },
+  });
+  const videoData = response.data;
+
+  const video: VideoProps = {
+    title: videoData.snippet.title,
+    publishTime: videoData.snippet.publishedAt,
+    description: videoData.snippet.description,
+    channelTitle: videoData.snippet.channelTitle,
+    imgUrl: videoData.snippet.thumbnails.high.url,
+    viewCount: (videoData.statistics.viewCount || 0) as number,
+  };
+  videosState.setState({ isFetching: false });
+  return video;
 };
