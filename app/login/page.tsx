f218bf30ad811b7966e37e56f54716003d502e2a
Hernan Valencia <frjr17@criptext.com>
2023-09-01 22:21:34 -0500

    feat: constructing login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,30 @@
-import { Center } from "@chakra-ui/react";
+"use client";
+import {
+  Button,
+  Center,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  LightMode,
+  VStack,
+} from "@chakra-ui/react";
 
 export default function Login() {
-  return <Center>Login Page</Center>;
+  return (
+    <Center>
+      <VStack>
+        <Heading>Sign In</Heading>
+        <FormControl>
+          <FormLabel>Email address</FormLabel>
+          <Input type="email" />
+          <FormHelperText>We&apos;ll never share your email.</FormHelperText>
+        </FormControl>
+        <LightMode>
+          <Button colorScheme="red">Sign In</Button>
+        </LightMode>
+      </VStack>
+    </Center>
+  );
 }
