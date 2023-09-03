c8f9f397b57f4b01ca0669e3be4bae8ec8736d8d
Hernan Valencia <frjr17@criptext.com>
2023-09-02 21:36:26 -0500

    feat: building and finishing banner responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,49 @@
+"use client";
+import {
+  Button,
+  Center,
+  HStack,
+  Heading,
+  LightMode,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      {/* banner */}
+      <Center
+        flexDirection={"column"}
+        height={{ base: "50vh", md: "75vh" }}
+        backgroundImage={"/images/clifford.webp"}
+        objectPosition={{ md: "0% 0%" }}
+        backgroundSize={"cover"}
+        width={"100%"}
+        alignItems={"start"}
+        paddingLeft={{ base: 25, md: 100 }}
+        gap={30}
+      >
+        <VStack align={"start"}>
+          <Heading fontSize={{ base: "4xl", md: "5xl" }}>
+            Clifford the Red Dog
+          </Heading>
+          <Text fontSize={{ base: "xl", md: "2xl" }} fontStyle={"italic"}>
+            A very interesting dog...
+          </Text>
+        </VStack>
+        <HStack fontSize={"xl"}>
+          <LightMode>
+            <Button>Play</Button>
+          </LightMode>
+          <Text>Add to List</Text>
+        </HStack>
+      </Center>
+      {/* slider one */}
+      {/* slider two */}
+      {/* slider three */}
     </>
   );
 }
